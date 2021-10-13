52c8e8746f69add8ea3d8b18ddadeabc8b353133
Arian <arian.kulmer@web.de>
2021-10-13 23:18:23 +0200

    renoir: Configure kernel
    Change-Id: I05d75fd1696993b7cf403c99d28df9a887c95f69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,5 +26,34 @@ TARGET_BOOTLOADER_BOARD_NAME := renoir
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+
+TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := vendor/renoir-qgki_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7350
+
+BOARD_KERNEL_CMDLINE := \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    cgroup.memory=nokmem,nosocket \
+    console=ttyMSM0,115200n8 \
+    ip6table_raw.raw_before_defrag=1 \
+    iptable_raw.raw_before_defrag=1 \
+    loop.max_part=7 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    pcie_ports=compat \
+    service_locator.enable=1 \
+    swiotlb=0 \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+
 # Include proprietary files
 include vendor/xiaomi/renoir/BoardConfigVendor.mk
