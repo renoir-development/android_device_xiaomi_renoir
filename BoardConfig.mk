9cd0a84fa9f9f0b346a4a277097505eaa8b86036
Arian <arian.kulmer@web.de>
2021-10-24 23:40:09 +0200

    Commonize more
    Change-Id: Ic84fd0a11c0ecf9d050e9270e8d49751af8ad389

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,81 +10,18 @@ DEVICE_PATH := device/xiaomi/renoir
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
 # Board
-TARGET_BOARD_PLATFORM := lahaina
 TARGET_BOOTLOADER_BOARD_NAME := renoir
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
 
 # HIDL
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest.xml
 
 # Kernel
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_SEPARATED_DTBO := true
-
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_BOOT_HEADER_VERSION := 3
-BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
-
-#TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
-#TARGET_FORCE_PREBUILT_KERNEL := true
-
-TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
-TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/renoir-qgki_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7350
 
-KERNEL_LD := LD=ld.lld
-
-BOARD_KERNEL_CMDLINE := \
-    androidboot.console=ttyMSM0 \
-    androidboot.hardware=qcom \
-    androidboot.memcg=1 \
-    androidboot.usbcontroller=a600000.dwc3 \
-    cgroup.memory=nokmem,nosocket \
-    console=ttyMSM0,115200n8 \
-    ip6table_raw.raw_before_defrag=1 \
-    iptable_raw.raw_before_defrag=1 \
-    loop.max_part=7 \
-    loop.max_part=7 \
-    lpm_levels.sleep_disabled=1 \
-    msm_rtb.filter=0x237 \
-    pcie_ports=compat \
-    service_locator.enable=1 \
-    swiotlb=0 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
-    androidboot.selinux=permissive
-
 # Partitions
-BOARD_BUILD_SUPER_IMAGE_BY_DEFAULT := true
-BOARD_USES_METADATA_PARTITION := true
-
-BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 201326592
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
-BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 114001162240
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 100663296
-
-BOARD_SUPER_PARTITION_GROUPS := qti_dynamic_partitions
-BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product system_ext system  vendor
-BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # (BOARD_SUPER_PARTITION_SIZE - 4194304) 4MiB overhead
-
-BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-
-TARGET_COPY_OUT_ODM := odm
-TARGET_COPY_OUT_SYSTEM_EXT := system_ext
-TARGET_COPY_OUT_VENDOR := vendor
-TARGET_COPY_OUT_PRODUCT := product
 
 # Include proprietary files
 #include vendor/xiaomi/renoir/BoardConfigVendor.mk
