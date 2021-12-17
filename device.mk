fd31e06cdd076ed0bae27a23c122b51e8b1e7a62
Erfan Abdi <erfangplus@gmail.com>
2021-12-17 22:37:11 +0330

    renoir: Build fingerprint hal service
    Change-Id: Ic204a3e0450f262bac8f2679591ca4b33c4f8f5a

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_shimaidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_mixer_paths_shimaidp.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_lahaina/sound_trigger_platform_info.xml
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
