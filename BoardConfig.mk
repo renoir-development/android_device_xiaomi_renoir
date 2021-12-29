8f34dead9d60b3720a2e52738fcae8eaa11bfac2
Arian <arian.kulmer@web.de>
2021-12-29 17:19:42 +0000

    renoir: Remove manifest and modules.load
    Change-Id: I7d6811bd2cdaf35822dc7780066726cdda6068f0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,15 +12,8 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := renoir
 
-# HIDL
-ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest.xml
-
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/renoir-qgki_defconfig
-
-# Kernel modules
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
 
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
