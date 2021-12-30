3e6f0bae155aff59e9dba131aa0dc86f4b0bf1fc
Arian <arian.kulmer@web.de>
2021-12-30 15:20:04 +0000

    Revert "Revert "renoir: overlay-lineage: Disable proximity usage during doze""
    This reverts commit 587f9d42323606f56493082c7febbcbb5136e3f6.

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
